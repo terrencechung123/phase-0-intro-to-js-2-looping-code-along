f9c6a4b36cc7f586e44d74e4a1dd5c0f101005d7
Terrence Chung <terrencechung35@gmail.com>
2023-01-11 10:09:04 -0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me`);
+   // debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+const cards = []
+
+function writeCards(name, eventName) {
+   
+    for(let i = 0; i < name.length; i++) {
+       
+        cards.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`)
+        
+    }
+    return cards
+}
+
+
+
+const countDownArray = []
+
+function countDown() {
+    let i = 10
+    while (i >= 0) {
+        console.log(i--)
+    }
+}
